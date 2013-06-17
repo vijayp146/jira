dc933398a709e9eb6d602dcb98452d6d2b055093
Andreas Hoffmann <furizaa@gmail.com>
2013-06-17 21:39:21 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jira::Application.config.secret_token = 'b9c983a1b3c2a0ef642ad6e79043ac8e09bccfcfee76ddc18d322a7a3e284e58c3503bcfc6aad74fa9303b632ed38234cd3b44014eeab2040ddb692e428d2265'
